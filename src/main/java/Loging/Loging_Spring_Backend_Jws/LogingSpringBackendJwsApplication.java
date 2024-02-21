0665a86de4c7b20b1779e8aacb05a0a10d26d473
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-02-21 16:50:26 -0500

    Initial commit

---
+++ src/main/java/Loging/Loging_Spring_Backend_Jws/LogingSpringBackendJwsApplication.java
@@ -0,0 +1,13 @@
+package Loging.Loging_Spring_Backend_Jws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogingSpringBackendJwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogingSpringBackendJwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Loging/Loging_Spring_Backend_Jws/LogingSpringBackendJwsApplicationTests.java
@@ -0,0 +1,13 @@
+package Loging.Loging_Spring_Backend_Jws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogingSpringBackendJwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
