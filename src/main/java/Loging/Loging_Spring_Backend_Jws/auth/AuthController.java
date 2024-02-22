f55d299de3cb02187fee38f6ae35217440c3c8da
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-02-22 11:01:47 -0500

    modificando funciones login register

--- src/main/java/Loging/Loging_Spring_Backend_Jws/auth/AuthController.java
+++ src/main/java/Loging/Loging_Spring_Backend_Jws/auth/AuthController.java
@@ -1,7 +1,9 @@
 package Loging.Loging_Spring_Backend_Jws.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class AuthController {
 
+    private final AuthService authService;
+
     @PostMapping(value = "login")
-    public String login(){
-        return "Login from public endpoint!";
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
     }
 
     @PostMapping(value = "register")
-    public String Register(){
-        return "Register from public endpoint!";
+    public ResponseEntity<AuthResponse> Register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authService.register(request));
     }
 
 }
---
+++ src/main/java/Loging/Loging_Spring_Backend_Jws/auth/AuthService.java
@@ -0,0 +1,17 @@
+package Loging.Loging_Spring_Backend_Jws.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
+public class AuthService {
+    public AuthResponse login(LoginRequest request) {
+        return null;
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        return null;
+    }
+}
