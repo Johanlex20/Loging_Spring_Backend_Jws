ad31dcccbf6fa22a27b496d782e3c2e6a76f7927
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-02-26 15:54:56 -0500

    Modificando el filtro de seguridad SecurityConfig

--- src/main/java/Loging/Loging_Spring_Backend_Jws/auth/AuthService.java
+++ src/main/java/Loging/Loging_Spring_Backend_Jws/auth/AuthService.java
@@ -6,6 +6,8 @@ import Loging.Loging_Spring_Backend_Jws.user.User;
 import Loging.Loging_Spring_Backend_Jws.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +24,12 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
 
     public AuthResponse login(LoginRequest request) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
     }
 
     public AuthResponse register(RegisterRequest request) {
